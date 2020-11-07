bf93b4212759eb4b132fc495820f6d4fedefc95b
MichiY3 <michiyoy@hotmail.com>
2020-11-07 15:23:16 +0900

    ハンバーガーメニュー追加

---
+++ welcome_page_yachi/js/main.js
@@ -0,0 +1,27 @@
+
+$(function(){}
+
+
+// ハンバーガーメニューが押された時
+$('#hamburger_btn').on('click',function(){
+	$(this).toggleClass('on');
+	$('drower .js-drower').toggleClass('on');
+	// 要素・hasClass('チェックしたいクラス名')
+	// チェックしたいクラス名がついていたらtrue ついてなければfalse
+	let isActive = $(this).hasClass('on');
+    toggleDrower(isActive);
+
+});
+
+
+
+function toggleDrower(isActive) {
+    if (isActive) {
+      // onになっていた時、メニューを表示
+      $('#drower-bg').fadeIn(600);
+    } else {
+      // onを外した時、メニューを非表示
+      $('#drower-bg').fadeOut(600);
+    }
+  } 
+
